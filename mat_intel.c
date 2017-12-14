68f1ccb7fbddb4ab652d437fb6a782303205b85a
patrickwu2 <patrickwu2@gmail.com>
2017-12-13 22:15:44 -0800

    fix bugs

--- mat_intel.c
+++ mat_intel.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include "mkl.h"
 #define max 8000
-double A[max][max] = {};
-double C[max][max] = {};
+double A[max*max] = {};
+double B[max*max] = {};
+double C[max*max] = {};
 	
 int main(){
 	for(int i = 0; i < max; i++)
 		for(int j = 0; j < max; j++)
-			A[i][j] = 1.00;
-	cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, max, max, max, 1, A, max, A, max, 0, C, max);
+			A[max*i+j] = B[max*i+j] = 1.00;
+	cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, max, max, max, 1, A, max, B, max, 0, C, max);
 	for(int i = 0; i < max; i++){
 		for(int j = 0; j < max; j++)
-			printf("%lf ", C[i][j]);
+			printf("%lf ", C[max*i+j]);
 		puts("");
 	}
 	return 0;
